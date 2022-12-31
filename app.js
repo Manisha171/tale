34a94a9a6af76bea72eef69268444aa01b190cce
Manisha171 <121050658+Manisha171@users.noreply.github.com>
2023-01-01 00:42:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require ('express');
+const app=express();
+app.get('/' ,function(req,res){
+    res.send("<h1>hello</h1><br><table border=2><tr><th>Name:</th><th>Work:</th><th>id:</th></tr></table>"
+    );
+})
+//app.get('/hello'),function(req,res){
+//console.log("calling rest function");
+//var person={firstName:'Manisha',lastName:"Singh"};
+//res.send(person);
+//}
+
+app.listen(8081);
+console.log("server is listening");
